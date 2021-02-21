b5a58977fe6044189cafcfaf14ef87a546b68258
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2021-02-21 17:08:02 +0100

    At least try to spell it correctly

--- madxtools/tablefs.py
+++ madxtools/tablefs.py
@@ -179,7 +179,7 @@ class TableFS:
                 self.Data["S"][i] += self.metaData["LENGTH"]
 
         if self.Data["S"][-1] == 0.0:
-            logger.warnning("Shifting last element from 0.0 to %d" % self.metaData["LENGTH"])
+            logger.warning("Shifting last element from 0.0 to %d" % self.metaData["LENGTH"])
             self.Data["S"][-1] = self.metaData["LENGTH"]
 
         # Kill elements array which is no longer valid
